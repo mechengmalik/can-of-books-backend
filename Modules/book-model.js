c61e6e831393c0243ba4b71408e90cba8f8431e1
MechEngMalik <mecheng.malik93@gmail.com>
2021-08-25 17:10:04 +0300

    atlas

--- Modules/book-model.js
+++ Modules/book-model.js
@@ -3,7 +3,7 @@
 const mongoose = require("mongoose");
 
 
-mongoose.connect('mongodb://localhost:27017/bestBooks', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 
